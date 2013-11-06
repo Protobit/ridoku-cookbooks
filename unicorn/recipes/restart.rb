dddbf708249842507e20082eb1d8a6776f9c08d0
Terry Meacham <zv1n.fire@gmail.com>
2013-11-06 12:31:27 -0600

    Add unicorn restart recipe and added environment handling into unicorn service script.

---
+++ unicorn/recipes/restart.rb
@@ -0,0 +1,9 @@
+# restart Unicorn service per app
+node[:deploy].each do |application, deploy|
+  execute "restart unicorn" do
+    command "#{deploy[:deploy_to]}/shared/scripts/unicorn restart"
+    only_if do
+      File.exists?("#{deploy[:deploy_to]}/shared/scripts/unicorn")
+    end
+  end
+end
