1aef010a9555c1b8642d89c151022ac04664cb57
Terry Meacham <zv1n.fire@gmail.com>
2014-08-14 01:57:23 -0500

    Ensure maintenance mode is only run on rails-app layers.

--- deploy/recipes/maintenance.rb
+++ deploy/recipes/maintenance.rb
@@ -1,21 +1,24 @@
 include_recipe 'rails::configure'
 
-# Set maintenance mode for an application instance.
-node[:deploy].each do |application, deploy|
-  if deploy[:application_type] == 'rails' &&
-    deploy.has_key?(:maintenance)
-    maint_file = "#{deploy[:current_path]}/public/maintenance.txt"
-    
-    file maint_file do
-      mode 0755
-      user 'deploy'
-      group 'www-data'
+if node[:opsworks][:instance][:layers].include?('rails-app')
 
-      case deploy[:maintenance]
-      when true, 'on', 'yes', 'true'
-        action = :create
-      else
-        action = :delete
+  # Set maintenance mode for an application instance.
+  node[:deploy].each do |application, deploy|
+    if deploy[:application_type] == 'rails' &&
+      deploy.has_key?(:maintenance)
+      maint_file = "#{deploy[:current_path]}/public/maintenance.txt"
+      
+      file maint_file do
+        mode 0755
+        user 'deploy'
+        group 'www-data'
+
+        case deploy[:maintenance]
+        when true, 'on', 'yes', 'true'
+          action = :create
+        else
+          action = :delete
+        end
       end
     end
   end
